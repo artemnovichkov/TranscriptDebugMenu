2f6052e051943f19aa8c0ee15601947fabfa347f
Artem Novichkov <mail@artemnovichkov.com>
2025-08-04 18:38:44 +0500

    Update docs

--- build-docc.sh
+++ build-docc.sh
@@ -4,6 +4,8 @@ export DOCC_JSON_PRETTYPRINT="YES"
 
 swift package --allow-writing-to-directory docs \
     generate-documentation --target TranscriptDebugMenu \
+    --transform-for-static-hosting \
+    --hosting-base-path TranscriptDebugMenu \
     --output-path docs
 
 echo '<script>window.location.href += "/documentation/transcriptdebugmenu"</script>' > docs/index.html
